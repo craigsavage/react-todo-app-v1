9530fbc25cf4b7f6c45fd7d9e7dbdb302a744c35
Craig <craig.savage@ontariotechu.net>
2021-10-17 06:49:32 -0400

    add styles and fixed UI of item list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,29 +7,29 @@ import TodoForm from "./TodoForm";
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
     id: null,
-    value: ""
+    value: "",
   });
 
-  const submitUpdate = value => {
-    updateTodo(edit.id, value)
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
     setEdit({
       id: null,
-      value: ""
-    })
-  }
+      value: "",
+    });
+  };
 
   if (edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
   return todos.map((todo, index) => (
     <div
       className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
+      onClick={() => completeTodo(todo.id)}
+      style={{ cursor: "pointer" }}
     >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
+      <div key={todo.id}>{todo.text}</div>
       <div className="icons">
         <RiCloseCircleLine
           onClick={() => removeTodo(todo.id)}
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 const TodoForm = (props) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleChange = (event) => {
     setInput(event.target.value);
@@ -15,22 +21,42 @@ const TodoForm = (props) => {
       text: input,
     });
 
-    setInput('');
+    setInput("");
   };
 
   return (
-    <form className='todo-form' onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Add a todo'
-        value={input}
-        name='text'
-        className='todo-input'
-        onChange={handleChange}
-      />
-      <button className='todo-button' type='submit'>
-        Add Todo
-      </button>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+          <input
+            type="text"
+            placeholder="Update your todo"
+            value={input}
+            name="text"
+            className="todo-input edit"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit" type="submit">
+            Update Todo
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button" type="submit">
+            Add Todo
+          </button>
+        </>
+      )}
     </form>
   );
 };
